238e1cebb8ae9a8adbf1609062a4a361d53a7fce
Suhel <jahidulsuhel@gmail.com>
2025-07-31 18:00:52 +0600

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
 
-      <h1>iMe Myself and I is enough for learnig ci cd/h1>
+      <h1>iMe Myself and I is enough for learnig ci cd</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
